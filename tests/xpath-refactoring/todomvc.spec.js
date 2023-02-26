0c7a7f9935c3ae5e8db515a5b2e30aaf34034c9e
SmallSparkle <a.kortikova@gmail.com>
2023-02-26 15:12:36 +0100

    added pageObg refactoring for todomvc test

---
+++ tests/xpath-refactoring/todomvc.spec.js
@@ -0,0 +1,24 @@
+import 'geckodriver';
+import { test, afterAll } from '@jest/globals';
+import { browser, perform, have } from 'selenidejs';
+import todomvc from '../../src/pages/todomvc.js';
+
+browser.config.browserName = 'firefox';
+
+test('completes todo', async () => {
+  await browser.open(todomvc.url);
+  await todomvc.addField.type('a').then(perform.pressEnter);
+  await todomvc.addField.type('b').then(perform.pressEnter);
+  await todomvc.addField.type('c').then(perform.pressEnter);
+  await todomvc.todoList.should(have.exactTexts('a', 'b', 'c'));
+
+  await todomvc.compliteTodoTasck('b').click();
+
+  await todomvc.listOfComplitedTacks().should(have.exactTexts('b'));
+  await todomvc.listOfNotComplitedTacks().should(have.exactTexts('a', 'c'));
+  await todomvc.todoList.should(have.exactTexts('a', 'b', 'c'));  
+});
+
+afterAll(async () => {
+  await browser.quit();
+});
