dd22f52008c7c97effe8f96c8ddccf3f1fcd10fe
SmallSparkle <a.kortikova@gmail.com>
2023-03-02 14:51:43 +0100

    removed debug parts

--- src/helpers/xpath.js
+++ src/helpers/xpath.js
@@ -5,19 +5,3 @@ function hasCssClass(name) {
 export default {
     hasCssClass 
 };
-
-const numbers = [2, 7, 19, 22, 38, 9];
-
-let i = 0;
-
-while (i <= numbers.length){
-    console.log(numbers[i]);
-
-    if (numbers[i] %2 >0){
-        numbers[i]*2;
-        i++
-    }
-    
-}
-
-console.log(numbers);
